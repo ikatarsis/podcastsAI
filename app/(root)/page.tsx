d50e943480a73054d0b90b53cc09f7225be6aa65
ekaterina.shevchenko <ekaterina.shevchenko@axenix.pro>
2024-07-24 14:52:32 +0300

    add auth with clerk and add user queries and schema db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,24 @@
+'use client';
 import React from 'react';
 import { podcastData } from '@/constants';
 import PodcastCard from '@/components/PodcastCard';
+import { useQuery } from "convex/react";
+import { api } from '@/convex/_generated/api';
 
 const Page = () => {
+	// const tasks = useQuery(api.tasks.get);
+
 	return (
 		<div className='mt-9 flex flex-col gap-9'>
-      <section className='flex flex-col gap-5'>
+			<section className='flex flex-col gap-5'>
 				<h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+
+				<div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+					{/*{tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}*/}
+				</div>
+
 				<div className='podcast_grid'>
-					{podcastData.map(({ id, title, description,imgURL }) => (
+					{podcastData.map(({ id, title, description, imgURL }) => (
 						<PodcastCard
 							key={id}
 							imgURL={imgURL}
@@ -18,7 +28,7 @@ const Page = () => {
 						/>
 					))}
 				</div>
-		</section>
+			</section>
 		</div>
 	);
 };
